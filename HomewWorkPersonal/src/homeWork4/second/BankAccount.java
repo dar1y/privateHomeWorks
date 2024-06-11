25c53918b62a4a7b1a3c8d7b23a74eb7961606c2
dar1y <60070626+dar1y@users.noreply.github.com>
2024-06-11 10:59:24 -0400

    all is done

--- HomewWorkPersonal/src/homeWork4/second/BankAccount.java
+++ HomewWorkPersonal/src/homeWork4/second/BankAccount.java
@@ -1,7 +1,34 @@
 package homeWork4.second;
-
 public class BankAccount {
-    void deposit(){}
-    void withdraw(){}
+
+    private int money;
+
+    synchronized void deposit(int amount) throws InterruptedException {
+        Thread.sleep(1000);
+        money += amount;
+        notifyAll();
+        System.out.println("Balance is increased " + money);
+    }
+
+    synchronized void withdraw(int amount) throws InterruptedException {
+        while (amount > money) {
+        }
+
+        System.out.println("Balance before withdrawal " + money);
+        money -= amount;
+        System.out.println("Balance after withdrawal " + money);
+    }
+
+    public BankAccount(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
 
--- HomewWorkPersonal/src/homeWork4/second/MyThread.java
+++ HomewWorkPersonal/src/homeWork4/second/MyThread.java
@@ -1,14 +1,37 @@
 package homeWork4.second;
 
-class MyThread extends Thread{
+import static homeWork4.second.MyThread.ba;
+
+class MyThread extends Thread {
+
     @Override
     public void run() {
-
+        try {
+            ba.deposit(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void main(String[] args) {
-        Thread thread1 = new Thread(new MyThread(),"thread1");
-        Thread thread2 = new Thread(new MyThread(),"thread2");
+    static BankAccount ba = new BankAccount(3000);
 
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(new MyThread(), "thread1");
+        Thread thread2 = new Thread(new MyThread1(), "thread2");
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class MyThread1 extends Thread {
+
+    @Override
+    public void run() {
+        try {
+            ba.withdraw(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
